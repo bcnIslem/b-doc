dc3c66e6f0962c4ab124a5209f67e616470716e7
islemwilcher <islemadobe6@gmail.com>
2022-06-29 15:05:55 +0100

    adding medical reducers to the app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,5 +4,6 @@ import auth from './auth'
 import patients from './patients'
 import checkUps from './checkUp'
 import treatments from './treatment'
+import medicalFolder from './medicalFolder'
 
-export const reducers = combineReducers({ auth, patients, checkUps, treatments })
+export const reducers = combineReducers({ auth, patients, checkUps, treatments, medicalFolder })
