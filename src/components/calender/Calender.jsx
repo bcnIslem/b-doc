22b1e7c0da2a0775deb5842819ab3ae1cf45b79a
islemwilcher <islemadobe6@gmail.com>
2022-07-10 20:52:59 +0100

    making some changes to get the current calender

--- src/components/calender/Calender.jsx
+++ src/components/calender/Calender.jsx
@@ -1,44 +1,41 @@
+import { DateRange } from "react-date-range";
+import { useEffect, useState } from "react";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { Grid } from "@material-ui/core";
 
-import { DateRange } from 'react-date-range';
-import {useEffect, useState} from 'react' 
-import 'react-date-range/dist/styles.css'; // main css file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-import { Grid } from '@material-ui/core';
-
-import useStyles from './styles'
-import { getPatientsByDate } from '../../actions/patient';
-import { useDispatch } from 'react-redux';
+import useStyles from "./styles";
+import { getPatientsByDate } from "../../actions/patient";
+import { useDispatch } from "react-redux";
 
 const Calander = ({ setPatientsDate }) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const classes = useStyles();
 
-    const classes = useStyles()
-
-    const [state, setState] = useState([
+  const [state, setState] = useState([
     {
-        startDate: new Date(),
-        endDate: null,
-        key: 'selection'
-    }
-    ]);
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
 
-    useEffect(() => {
+  useEffect(() => {
+    dispatch(getPatientsByDate(state, setPatientsDate));
+  }, [state]);
 
-        dispatch(getPatientsByDate(state, setPatientsDate))
-    }, [state])
+  return (
+    <Grid className={classes.calender}>
+      <DateRange
+        className={classes.component}
+        editableDateInputs={true}
+        onChange={(item) => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        ranges={state}
+      />
+    </Grid>
+  );
+};
 
-    return (
-        <Grid className={classes.calender} elevation={3}>
-            <DateRange
-                className={classes.component}
-                editableDateInputs={true}
-                onChange={item => setState([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={state}
-            />
-        </Grid>
-    );
-}
-
-export default Calander
+export default Calander;
--- src/components/calender/styles.js
+++ src/components/calender/styles.js
@@ -8,7 +8,7 @@ export default makeStyles((theme) => ({
     },
     component: {
         width: '100%',
-        height: '320px',
+        height: '350px',
     },
 
     [theme.breakpoints.down('lg')]: {
