a36f7c99c723bf0339e95a69d9f80b06a06006f8
islemwilcher <islemadobe6@gmail.com>
2022-07-10 21:00:09 +0100

    creating the medical folder add page

--- src/pages/addMedicalFolder/AddMEdicalFolder.jsx
+++ src/pages/addMedicalFolder/AddMEdicalFolder.jsx
@@ -1,104 +1,135 @@
-
 // hooks
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { useSelector, useDispatch } from "react-redux"
-import { useTranslation } from "react-i18next"
-import ReactImageFileToBase64 from "react-file-image-to-base64"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import ReactImageFileToBase64 from "react-file-image-to-base64";
 
 // components
-import Header from "../../components/header/Header"
+import Header from "../../components/header/Header";
 
 // actions
-import { addMedicalFolder } from "../../actions/medicalFolder"
+import { addMedicalFolder } from "../../actions/medicalFolder";
 
 // material ui components
-import { Container, Paper, Grid, Typography, TextField, Button } from '@material-ui/core'
-import { IconButton } from '@material-ui/core'
+import {
+  Container,
+  Paper,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+} from "@material-ui/core";
 
-// icons
-import { MdOutlineRemove, MdAdd } from 'react-icons/md'
-
-import useStyles from './styles'
+import useStyles from "./styles";
 
 const AddMEdicalFolder = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const classes = useStyles();
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const classes = useStyles()
-    const { t } = useTranslation()
+  const patientToAddMedicalFolder = useSelector(
+    (state) => state.patients.patientToAddMedicalFolder
+  );
 
-    const patientToAddMedicalFolder = useSelector((state) => state.patients.patientToAddMedicalFolder)
+  const [folderData, setFolderData] = useState([]);
 
-    const [folderData, setFolderData] = useState([])
+  const [medicalFolderData, setMedicalFolderData] = useState({
+    patientId: "",
+    title: "",
+    folder: [],
+  });
 
-    const [medicalFolderData, setMedicalFolderData] = useState({
-        patientId: '',
-        title: '',
-        folder: []
-    })
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    medicalFolderData.patientId = patientToAddMedicalFolder.patientId;
 
-        medicalFolderData.patientId = patientToAddMedicalFolder.patientId;
+    //setMedicalFolderData({ ...medicalFolderData, folders: folderData });
+    medicalFolderData.folder.push(folderData);
+    console.log("data:", medicalFolderData);
+    dispatch(addMedicalFolder(medicalFolderData, navigate));
+    clear();
+  };
 
-        //setMedicalFolderData({ ...medicalFolderData, folders: folderData });
-        medicalFolderData.folder.push(folderData);
+  const handleOnCompleted = (files) => {
+    setFolderData({ ...folderData, files });
+  };
 
-        console.log('medical folder: ', medicalFolderData)
+  const clear = () => {
+    setFolderData([]);
 
-        dispatch(addMedicalFolder(medicalFolderData, navigate));
-        clear()
-    }
+    setMedicalFolderData({
+      patientId: "",
+      title: "",
+      folder: [],
+    });
+  };
 
-    const handleOnCompleted = files => {
-        files.map((file) => {
-            folderData.push(file.base64_file)
-        })
+  return (
+    <Container>
+      <Header />
+      <Paper className={classes.paper} elevation={3}>
+        <form
+          autoComplete="off"
+          noValidate
+          className={classes.form}
+          onSubmit={handleSubmit}
+        >
+          <Typography variant="h3" className={classes.title}>
+            Add Medical Folder
+          </Typography>
+          <Typography variant="h4">
+            {patientToAddMedicalFolder.fullName}
+          </Typography>
 
-        console.log('folder:', folderData)
-    };
+          <Paper className={classes.input}>
+            <Grid className={classes.folder}>
+              <TextField
+                className={classes.textfield}
+                name="title"
+                variant="outlined"
+                fullWidth
+                label="folder title"
+                value={medicalFolderData.title}
+                onChange={(e) =>
+                  setMedicalFolderData({
+                    ...medicalFolderData,
+                    title: e.target.value,
+                  })
+                }
+              />
+              {/* <div className={classes.fileInput}>
+                <input type="file" multiple={true} onChange={handleImage} />
+                {/* <FileBase
+                  type="file"
+                  multiple={false}
+                  onDone={({ base64 }) => console.log("base:", base64)}
+                />
+              </div> */}
+              <div className={classes.folderBtn}>
+                {" "}
+                <ReactImageFileToBase64
+                  multiple={true}
+                  onCompleted={handleOnCompleted}
+                />
+              </div>
+            </Grid>
+          </Paper>
+          <Grid className={classes.actions}>
+            <Button
+              type="submit"
+              className={classes.btnSubmit}
+              variant="contained"
+              size="large"
+              fullWidth
+            >
+              add folder
+            </Button>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
 
-    const clear = () => {
-        setFolderData([])
-
-        setMedicalFolderData({
-            patientId: '',
-            title: '',
-            folder: []
-        })
-    }
-    return (
-        <Container>
-            <Header />
-            <Paper className={classes.paper} elevation={3}>
-                <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
-                <Typography variant='h3' className={classes.title}>Add Medical Folder</Typography> 
-                <Typography variant='h4'>{patientToAddMedicalFolder.fullName}</Typography>
-
-                <Paper className={classes.input} >
-                    <Grid className={classes.folder}>
-                        <TextField className={classes.textfield}
-                            name='title'
-                            variant='outlined'
-                            fullWidth
-                            label='folder title'
-                            value={medicalFolderData.title}
-                            onChange={(e) => setMedicalFolderData({ ...medicalFolderData, title: e.target.value })}
-                        />
-                        <div className={classes.folderBtn}>
-                        <ReactImageFileToBase64 multiple={true} onCompleted={handleOnCompleted} />
-                        </div>
-                    </Grid>
-                </Paper>
-                <Grid className={classes.actions}>
-                    <Button type='submit' className={classes.btnSubmit} variant='contained' size='large' fullWidth>add folder</Button>
-                </Grid>
-                </form>
-            </Paper>
-        </Container>
-    )
-}
-
-export default AddMEdicalFolder
+export default AddMEdicalFolder;
